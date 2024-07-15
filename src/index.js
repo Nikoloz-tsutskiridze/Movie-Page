bf322e6a42f22193dc6a219b18b47de188c91ee0
Nikoloz Tsutskiridze <nikacucqiridze2000@gmail.com>
2024-07-15 21:53:24 +0400

    Movie-App

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import App from "./App";
+// import "./index.css";
+import StartRating from "./StarRating";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <StartRating maxRating={5} />
+    <StartRating maxRating={10} />
+  </React.StrictMode>
+);
